00b6f4c63840b111a680fe3e686508670e10e04c
ibalt <brezhev@ya.ru>
2025-02-16 05:00:37 +0300

    correct_1

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -5,9 +5,48 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import csv
+import datetime as dt
+from collections import defaultdict
+
+from scrapy.exceptions import DropItem
+
+from pep_parse.settings import BASE_DIR, DATETIME_FORMAT, RESULTS_DIR
 
 
 class PepParsePipeline:
+    """
+    Pipeline для подсчета статусов PEP и сохранения результатов в CSV-файл.
+    """
+    def open_spider(self, spider):
+        """
+        Инициализирует словарь для подсчета статусов PEP при открытии паука.
+        """
+        self.pep_status_counts = defaultdict(int)
+
     def process_item(self, item, spider):
+        """Обрабатывает каждый PEP и подсчитывает его статус."""
+        pep_status = item.get('status')
+        if pep_status is None:
+            raise DropItem(f'Неизвестный статус pep - {item}')
+        self.pep_status_counts[pep_status] += 1
         return item
+
+    def close_spider(self, spider):
+        """
+        Сохраняет результаты подсчета статусов PEP в CSV-файл
+        при закрытии паука.
+        """
+        results_dir = BASE_DIR / RESULTS_DIR
+        results_dir.mkdir(exist_ok=True)
+
+        results = [('Статус', 'Количество')]
+        status_count_pairs = list(self.pep_status_counts.items())
+        results.extend(status_count_pairs)
+        results.append(('Total', sum(self.pep_status_counts.values())))
+        now_format_time = dt.datetime.now().strftime(DATETIME_FORMAT)
+        file_name = f'status_summary_{now_format_time}.csv'
+        file_path = results_dir / file_name
+        with open(file_path, 'w', encoding='utf-8') as f:
+            writer = csv.writer(f, dialect='unix')
+            writer.writerows(results)
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,8 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+from pep_parse.settings import PEP_ALLOWED_DOMAINS, PEP_NAME, PEP_START_URLS
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +10,24 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        """
+        Обрабатывает главную страницу и извлекает ссылки на отдельные PEP.
+        """
+        peps = response.css('a.pep.reference.internal::attr(href)').getall()
+        for pep in peps:
+            yield response.follow(pep, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """
+        Парсит страницу PEP и извлекает номер, название и статус.
+        """
+        number, name = (
+            response.css('h1.page-title::text').get().split(' – ', 1)
+        )
+
+        data = {
+            'number': number,
+            'name': name,
+            'status': response.css('abbr::text').get(),
+        }
+        yield PepParseItem(data)
